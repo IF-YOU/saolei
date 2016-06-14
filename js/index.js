04bc047bdc17eeaa0a8abf6edf149add2f3df5af
胡鑫鑫 <870895354@qq.com>
2016-06-14 11:32:57 +0800

    111
    1111

--- js/index.js
+++ js/index.js
@@ -1,157 +1,138 @@
 $(function(){
-  var num = 0.99,
-  data=0,
-  timerid;
-  var time = function(){
-    data+=1;
-    $('.time').text(data);
-  }
-  $(document).on('contextmenu',false);
-  $('.begin').on('click',function(){
-    timerid=setInterval(time,1000) 
-    $('.sweep').empty()
-    $('.action').slideUp()
-    render()
-  })
-  $('.restart').on('click',function(){     
-    $('.sweep').empty()      
-    $('.btn').slideUp()
-    data=0;
-    timerid=setInterval(time,1000) 
-    render()    
-  })
+	var hang=20;
+	var she=[{x:0,y:0},{x:0,y:1},{x:0,y:2}];
+	var shiwu={};
+	var timerId;
+	var fangxiang=39;
 
-  $('.ul li[data-id]').on('click',function(){
-    $('.active').removeClass('active')
-    $(this).addClass('active')
-    $('.sweep').empty()
-    num = Number($(this).attr('data-id'))     
-    render()
-    data=0;
-    setInterval(timerid,1000)
-  })
+	var zb2id=function(x,y){
+		return x+'-'+y;
+	}
 
-  $('.sweep').on('mouseenter',function(){
-    $(this).css({cursor:"url(./image/we.png),auto"})
-  })
-
-  var xy2id = function(a,b){
-   return a+'-'+b
- }
- var jisuan = function(x,y){
-   var num = 0;
-   if($('#'+xy2id(x,y-1)).is('.lei')){num++};
-   if($('#'+xy2id(x-1,y-1)).is('.lei')){num++};
-   if($('#'+xy2id(x+1,y-1)).is('.lei')){num++};
-   if($('#'+xy2id(x+1,y+1)).is('.lei')){num++};
-   if($('#'+xy2id(x,y+1)).is('.lei')){num++};
-   if($('#'+xy2id(x+1,y)).is('.lei')){num++};
-   if($('#'+xy2id(x-1,y)).is('.lei')){num++};
-   if($('#'+xy2id(x-1,y+1)).is('.lei')){num++};
-   return num;
- }
-
- var youjian = function(e){
-   if($(this).is('.tip')){
-    return
-  }
-  $(this).toggleClass('biaoji')
-  if($('.biaoji.lei').length === $('.lei').length){
-   alert('你太棒了！！！')
-   $('.sweep').empty();
-   render()
-   data=0;
- }
-}
-
-var clickHandler = function(e){
- if (e.which === 1) {
-  $.proxy(zuojian,this)(e)
-}else if(e.which === 3){
-  $.proxy(youjian,this)(e)
-}
-}
-
-function render(){    
-  for(var i=0;i<10;i++){
-    for(var j=0;j<10;j++){
-     var isLei = Math.random() > num;
-     $('<div>')
-     .attr('id',i+'-'+j)
-     .on('mousedown',{x:i,y:j,lei:isLei},clickHandler)
-     .addClass(function(){
-      return 'block '+(isLei ? 'lei':'');
-    })
-     .appendTo('.sweep')
-   }
- }
-}
-render()
+	//画场景
+	var huachangjing=function(){			
+		var i=0,j=0,sence=$('#sence'),wh=Math.floor(600/hang);
+		hang=hang||20;
+		sence.empty();
+		for(;i<hang;i++){
+			for(j=0;j<hang;j++){
+				$('<div>').addClass('block')
+				.attr('id',zb2id(i,j))
+				.width(wh-1)
+				.height(wh-1)
+				.appendTo('#sence');
+			}
+		}
+		sence.width(wh.hang).height(wh*hang);
+	}
+	huachangjing();
 
 
-var finding=function  (x,y) {
-  var num=0;
-  for(var i=x-1;i<=x+1;i++){
-    for(var j=y-1;j<=y+1;j++){
-      if($('#'+xy2id(i,j)).is('.lei')){
-        num+=1;
-      }
+    // 画蛇
+    var zidianshe={'0-0':true,'0-1':true,'0-2':true};
+    var fangshe=function(){
+    	she.forEach(function(v){
+    		$( '#'+ zb2id(v.x,v.y)).addClass('she');
+    	});  
     }
-  }
-  return num;
-}
+    fangshe();
+    she.forEach(function(v){
+    	$('#'+zb2id(v.x,v.y)).addClass('she');
+    });
 
-var digui=function  (x,y) {
-  var mine=finding(x,y);
-  if(mine===0){
-    for(var i = x - 1;i <= x + 1;i++){
-      for(var j = y - 1;j <= y + 1;j ++) {
-                      //越界
-                      if(i>9||i<0||j<0||j>9){
-                       continue;
-                     }
-                      //自己
-                      if(i == x && j == y){
-                       continue;
-                     }
-                      //已遍历
-                      if($('#'+xy2id(i,j)).hasClass('tip')){
-                       continue;
-                     }
-
-                     mine=finding(i,j);
-                     $('#'+xy2id(i,j)).addClass('tip').text(mine);
-                     if(mine===0){
-                      $('#'+xy2id(i,j)).text('')
-                    }
-
-                    digui(i,j);
-                  }
-                }
-              }
-            } 
+    // 放食物
+	var fangshiwu=function(){
+		var _x=Math.floor(Math.random()*hang);
+		var _y=Math.floor(Math.random()*hang);
+		$('#'+zb2id(_x,_y)).addClass('shiwu');
+		return{x:_x,y:_y};
+	}
+	shiwu=fangshiwu();
 
 
+	// 移动
+	var move=function(){
+		var jiutou=she[she.length-1];
+		if(fangxiang===39){
+			var xintou={x:jiutou.x,y:jiutou.y+1};
+		}else if(fangxiang===40){
+			var xintou={x:jiutou.x+1,y:jiutou.y};
+		}else if(fangxiang===37){
+			var xintou={x:jiutou.x,y:jiutou.y-1};
+		}else if(fangxiang===38){
+			var xintou={x:jiutou.x-1,y:jiutou.y};
+		} 
 
-    //左击3种状态
-    var zuojian=function  (e) {
-      if($(this).is('.biaoji')){
-        return;
-      }else if(e.data.lei){
-        $('.lei').addClass('fail');
-        $('.btn').slideDown();
-        data=0;
-        clearInterval(timerid);                
-      }else{
-        var mine=finding(e.data.x,e.data.y);
-        $(this).addClass('tip').text(mine);
-        if(mine===0){
-          $(this).text('')
-        }
-        digui(e.data.x,e.data.y)
-      }
-    }
-  })
+		/// 撞墙了
+		if(xintou.x<0||xintou.x>hang-1||xintou.y<0||xintou.y>hang-1){
+			clearInterval(timerId);
+			return;
+			// 吃食物
+		}else if(xintou.x===shiwu.x&&xintou.y===shiwu.y){
+			she.push(xintou);
+			$('#'+zb2id(xintou.x,xintou.y)).addClass('she');
+			$('.shiwu').removeClass('shiwu');
+			shiwu=fangshiwu();
+			// 没有吃到食物
+		}else{
+			she.push(xintou);
+			var weiba=she.shift();			
+			$('#'+zb2id(xintou.x,xintou.y)).addClass('she');
+			$('#'+zb2id(weiba.x,weiba.y)).removeClass("she");
+		}
+
+
+		$(document).bind('keydown',function(e){
+			if(e.keyCode<37||e.keyCode>40){
+				return;
+			}else{
+				fangxiang=e.keyCode;
+			}			
+		})
+	}
+			
 
 
 
+	// 点击选择行列
+	$('ul li[data-guanka]').bind('click',function(){
+		var guanka=Number($(this).attr('data-guanka'));
+		if(guanka==='chuji'){
+			setInterval(move,400);
+		}else if(guanka==='zhongji'){
+			setInterval(move,200);
+		}else if(guanka==='gaoji'){
+			setInterval(move,50);
+		}
+		$('.active').removeClass('active');		
+		huachangjing();
+		fangshe();
+		fangshiwu();
+		$(this).addClass('active');
+	})
+
+	
+
+
+	// 开关
+	$('ul li[data-cotroll]').bind('click',function(){
+		var str=$(this).attr('data-cotroll');
+		if(str==='kaishi'){
+			timerId=setInterval(move,200);
+		}else if(str==='zanting'){
+			clearInterval(timerId);
+		}else if(str==='restart'){
+			clearInterval(timerId);
+			huachangjing();
+			she=[{x:0,y:0},{x:0,y:1},{x:0,y:2}];
+			shiwu=fangshiwu();
+			she.forEach(function(v){
+				$('#'+zb2id(v.x,v.y)).addClass('she');
+			});
+			setInterval(move,200);
+		}
+	})
+
+
+
+})
